5369a555cff6b74bc8aa60b40eaa35aa470e6bb6
Євгеній Москаленко <moskale2yevhenii@hneu.net>
2019-12-17 21:19:53 +0300

    footer and form

--- src/views/index.vue
+++ src/views/index.vue
@@ -419,7 +419,7 @@
               </div>
             </div>
             <div class="col-6">
-              <div class="comment mt-5">
+              <div class="comment mt">
                 <div class="row">
                   <div class="col-3">
                     <img src="../assets/Base.png" alt />
@@ -435,7 +435,7 @@
               </div>
             </div>
             <div class="col-6">
-              <div class="comment mt-5">
+              <div class="comment mt">
                 <div class="row">
                   <div class="col-3">
                     <img src="../assets/Base.png" alt />
@@ -451,6 +451,49 @@
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+    <section class="get__tuach">
+      <div class="container">
+        <div class="touch">
+          <h2>GET IN TOUCH</h2>
+          <p>1600 Pennsylvania Ave NW, Washington, DC 20500, United States of America. Tel: (202) 456-1111</p>
+          <div class="touch__form">
+            <form>
+              <div class="row">
+                <div class="col">
+                  <div class="form_group">
+                    <input type="text" class="form-control bgc" placeholder="Your Name *" />
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="form_group">
+                    <input type="text" class="form-control bgc" placeholder="Your Name *" />
+                  </div>
+                </div>
+              </div>
+              <div class="form_group">
+                <textarea class="form-control textr bgc" placeholder="Your Name *"></textarea>
+              </div>
+              <div class="d-flex justify-content-center">
+                <button class="send_message">SEND MESSAGE</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="footer d-flex justify-content-center align-items-center">
+      <div class="container">
+        <div class="footer__link">
+          <router-link to="/">Facebook</router-link>
+          <router-link to="/">Twitter</router-link>
+          <router-link to="/">Google+</router-link>
+          <router-link to="/">Linkedln</router-link>
+          <router-link to="/">Behance</router-link>
+          <router-link to="/">Dribbble</router-link>
+          <router-link to="/">GitHab</router-link>
         </div>
       </div>
     </section>
@@ -827,5 +870,87 @@ export default {
 }
 .people__comment {
   margin-top: 40px;
+}
+
+.mt {
+  margin-top: 140px;
+}
+.people__content {
+  margin-bottom: 40px;
+}
+.get__tuach {
+  width: 100%;
+  height: 800px;
+  background-color: #3c5499;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+}
+.touch {
+  h2 {
+    text-align: center;
+    font-size: 40px;
+    font-weight: 700;
+    line-height: 29.91px;
+    text-transform: uppercase;
+  }
+  h2::after {
+    content: "";
+    display: block;
+    margin: 20px auto;
+    width: 87px;
+    height: 4px;
+    background: rgba(0, 0, 0, 0.155);
+  }
+  p {
+    text-align: center;
+    font-size: 18px;
+    font-weight: 400;
+  }
+}
+.textr {
+  margin-top: 20px;
+  height: 200px;
+}
+.bgc {
+  background-color: #273a71;
+  border: none;
+  color: #fff;
+  padding: 20px 15px;
+}
+.send_message {
+  margin-top: 60px;
+  background-color: #30bae7;
+  border: none;
+  color: #fff;
+  padding: 20px 80px;
+  text-transform: uppercase;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.308);
+  border-radius: 3px;
+}
+.touch__form {
+  margin-top: 75px;
+}
+.footer {
+  width: 100%;
+  height: 112px;
+  background: #344b8e;
+}
+.footer__link {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  a {
+    font-size: 24px;
+    font-weight: 400;
+    color: #ffffff;
+    line-height: 29.91px;
+    text-decoration: none;
+    padding: 0px 25px;
+  }
+  a:hover {
+    color: #30bae7;
+  }
 }
 </style>
